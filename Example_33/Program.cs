006ee7361e3648ad8cf71ae9a057031ba69c5f80
Kirill <ganaboss66@gmail.com>
2023-03-23 12:26:13 +0300

    Выбрали число из массива

---
+++ Example_33/Program.cs
@@ -0,0 +1,25 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next(-9, 10);
+}
+
+string SearchNumberInArray(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+          return "yes";
+    }
+    return "no";
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.Write("Введите число: ");
+int k = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SearchNumberInArray(array, k));
