3b7ce2d51a3edd6a120013fad922fbfada6b20d8
Kirill <ganaboss66@gmail.com>
2023-03-23 13:27:25 +0300

    перемножили элементы массива

---
+++ Example_37/Program.cs
@@ -0,0 +1,18 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next(1, 10);
+}
+
+void ProizvedenieElements(int[] array)
+{
+  for (int i = 0; i < array.Length / 2 + array.Length % 2; i++)
+    Console.Write($"{array[i] * array[array.Length - 1 - i]} ");
+}
+
+Console.Clear();
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ProizvedenieElements(array);
