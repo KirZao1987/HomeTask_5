691e344cddace8623157d77af770fcb87b9e1574
Kirill <ganaboss66@gmail.com>
2023-03-23 16:24:38 +0300

    не смог решить

---
+++ HomeTask_38/Program.cs
@@ -0,0 +1,21 @@
+﻿void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next(1, 100);
+}
+double max = array[0];
+double min = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+  if (array[i] > max)
+  {
+    max = array[i];
+  }
+  if (array[i] < min)
+  {
+    min = array[i];
+  }
+}
+double diff = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным элементами {diff}");
